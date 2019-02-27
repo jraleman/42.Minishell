01fd9ba286a0a71df5e954836f1b6f5b4a594192
jraleman <jraleman@bendiburg.com>
2019-02-27 10:50:04 -0800

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,10 +1,55 @@
-void    sh_loop(void)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaleman <jaleman@student.42.us.org>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/25 10:22:32 by jaleman           #+#    #+#             */
+/*   Updated: 2019/02/25 10:22:33 by jaleman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include "minishell.h"
+
+/*
+** Shell looping function
+*/
+
+int			sh_loop(char *argv[])
 {
-    return ;
+	int		ret;
+	int		loop;
+	char	*line;
+	char	**args;
+
+	ret = 0;
+	loop = 1;
+	while (loop)
+	{
+		printf("🍍 ");
+
+		free(line);
+		free(args);
+	}
+	return (ret);
 }
 
-int     main(int argc, char *argv[])
+// -----------------------------------------------------------------------------
+
+
+/*
+** Main program function
+*/
+
+int			main(int argc, char *argv[])
 {
-    sh_loop();
-    return (0);
+	int		ret;
+
+	ret = 0;
+	if (argc == 1)
+		ret = sh_loop(NULL);
+	else if (argc == 2)
+		ret = sh_loop(argv[1]);
+	return (ret);
 }
