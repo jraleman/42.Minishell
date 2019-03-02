d384d416470b82d27f9472809ffe2f690a023883
jraleman <jraleman@bendiburg.com>
2019-03-02 08:53:10 -0800

    Add all needed header files

--- src/main.c
+++ src/main.c
@@ -10,12 +10,29 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "minishell.h"
-// fork(); execvp();   |||||||||    execve();
+#include <sys/wait.h>
+    // waitpid() and associated macros
 #include <unistd.h>
-#include <stdio.h>
+    // chdir()
+    // fork()
+    // exec()
+    // pid_t
 #include <stdlib.h>
+    // malloc()
+    // realloc()
+    // free()
+    // exit()
+    // execvp()
+    // EXIT_SUCCESS, EXIT_FAILURE
+#include <stdio.h>
+    // fprintf()
+    // printf()
+    // stderr
+    // getchar()
+    // perror()
 #include <string.h>
+    // strcmp()
+    // strtok()
 
 /*
 ** Macros
