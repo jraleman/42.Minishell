8c825b5f5092618a18dbf2b2ae54f61a4efd29fa
jraleman <jraleman@bendiburg.com>
2019-03-04 02:56:34 -0800

    Implement cmd_cd func

---
+++ src/builtins/cmd/cmd_cd.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cmd_exit.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaleman <jaleman@student.42.us.org>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/25 10:22:32 by jaleman           #+#    #+#             */
+/*   Updated: 2019/02/25 10:22:33 by jaleman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+/*
+** ...
+*/
+
+int			cmd_cd(char **args)
+{
+	if (!args[1])
+		fprintf(stderr, "%s: expected argument to \"cd\"\n", g_app);
+	else
+		if (chdir(args[1]))
+			perror(g_app);
+	return (1);
+}
