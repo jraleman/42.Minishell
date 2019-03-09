22f6ca87419934841a0b7b4eed9e88fdec0a858b
jraleman <jraleman@bendiburg.com>
2019-03-09 11:57:13 -0800

    Implement verify_flags func and helpers

--- src/main.c
+++ src/main.c
@@ -16,30 +16,73 @@
 ** Print usage message.
 */
 
-static int	print_usage(char *bin)
+static int		print_usage(char *bin)
 {
 	printf("usage: %s [option]\n", bin);
-	return (EXIT_SUCCESS);
+	return (EXIT_FAILURE);
 }
 
 /*
-** Main program function
+** Set invalid flag.
+*/
+
+static int		invalid_flag(char *bin, char opt)
+{
+	printf("%s: illegal option -- %c\n", bin, opt);
+	return (0);
+}
+
+/*
+** Validate flag.
+*/
+
+static int		validate_flag(char *bin, char *flag)
+{
+	int			i;
+	int			ret;
+
+	i = 0;
+	ret = 1;
+	while (ret == 1 && flag[++i])
+		if (!ft_cisin(OPTS_FLGS, flag[i]))
+			ret = invalid_flag(bin, flag[i]);
+	return (ret);
+}
+
+/*
+** Verify flags.
+*/
+
+static int		verify_flags(char *bin, char *flags[], int total)
+{
+	int			i;
+
+	i = 0;
+	while (++i < total)
+	{
+		if (flags[i][0] == '-' && validate_flag(bin, flags[i]))
+			continue ;
+		break ;
+	}
+	return (i == total);
+}
+
+/*
+** Main function.
 */
 
 int			main(int argc, char *argv[])
 {
 	int		ret;
-	char	*bin;
-	char	*opt;
+	int		valid;
 
 	ret = EXIT_SUCCESS;
-	bin = argv[0];
-	opt = argv[1];
+	valid = verify_flags(argv[0], argv, argc);
 	if (argc == 1)
-		ret = minishell(bin, NULL);
-	else if (argc == 2)
-		ret = minishell(bin, opt);
+		ret = minishell(argv[0], NULL);
+	else if (valid && argc > 1)
+		ret = minishell(argv[0], argv);
 	else
-		ret = print_usage(bin);
+		ret = print_usage(argv[0]);
 	return (ret);
 }
