15a05702cd8a42551e1eeebafc3d10811a2dd68b
jraleman <jraleman@bendiburg.com>
2019-04-03 16:12:49 -0700

    Add util funcs

---
+++ src/utils/ft_strlen.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaleman <jaleman@student.42.us.org>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/02 16:00:17 by jaleman           #+#    #+#             */
+/*   Updated: 2016/11/02 16:00:19 by jaleman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "utils.h"
+
+/*
+** Global variables for the masks.
+*/
+
+static const unsigned long g_mask01 = 0x0101010101010101;
+static const unsigned long g_mask80 = 0x8080808080808080;
+
+/*
+** Test the byte of the pointer.
+** If at least one bit is a null character, returns that position.
+** If none is found, returns -1.
+*/
+
+static int	test_byte(const char *ptr)
+{
+	int	ret;
+
+	ret = -1;
+	if (ptr[0] == '\0')
+		ret = 0;
+	else if (ptr[1] == '\0')
+		ret = 1;
+	else if (ptr[2] == '\0')
+		ret = 2;
+	else if (ptr[3] == '\0')
+		ret = 3;
+	else if (ptr[4] == '\0')
+		ret = 4;
+	else if (ptr[5] == '\0')
+		ret = 5;
+	else if (ptr[6] == '\0')
+		ret = 6;
+	else if (ptr[7] == '\0')
+		ret = 7;
+	return (ret);
+}
+
+/*
+** Computes the length of the string s.
+*/
+
+size_t		ft_strlen(const char *s)
+{
+	int					x;
+	const char			*ptr;
+	const unsigned long	*lptr;
+
+	x = 0;
+	ptr = s;
+	while (((uintptr_t)ptr & sizeof(long)) - 1)
+	{
+		if (*ptr == '\0')
+			return (ptr - s);
+		ptr += 1;
+	}
+	lptr = (const unsigned long *)ptr;
+	while (1)
+	{
+		if ((*lptr - g_mask01) & g_mask80)
+		{
+			ptr = (const char *)(lptr);
+			if ((x = test_byte(ptr)) >= 0)
+				break ;
+		}
+		lptr += 1;
+	}
+	return (ptr - s + x);
+}
---
+++ src/utils/ft_strncat.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaleman <jaleman@student.42.us.org>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/03 18:55:02 by jaleman           #+#    #+#             */
+/*   Updated: 2016/11/03 18:55:04 by jaleman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "utils.h"
+
+/*
+** Appends not more than n characters from the string s2, to the end of the
+** null-terminated string s1, then add a terminating '/0'. The string s1
+** must have sufficient space to hold the result.
+*/
+
+char	*ft_strncat(char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = ft_strlen(s1);
+	while ((i < n) && s2[i])
+	{
+		s1[i + j] = s2[i];
+		i += 1;
+	}
+	s1[i + j] = '\0';
+	return (s1);
+}
