4b4d10cbcc6c944b507c5762c73b0ee4c013f5c4
jraleman <jraleman@bendiburg.com>
2019-04-04 19:21:49 -0700

    Implement env builtin

---
+++ src/builtins/cmd_env.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cmd_env.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaleman <jaleman@student.42.us.org>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/25 10:22:32 by jaleman           #+#    #+#             */
+/*   Updated: 2019/02/25 10:22:33 by jaleman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int		cmd_env(char **args, char **env, char *name)
+{
+	int		i;
+
+	(void)args;
+	(void)name;
+	i = 0;
+	while (env[i] != NULL)
+		printf("%s\n", env[i]);
+	return (1);
+}
