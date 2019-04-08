fca46d91915307c39520b6d80f0d679b7f38ab45
jraleman <jraleman@bendiburg.com>
2019-04-08 12:00:17 -0700

    Implement tablen and print_welcome func

--- src/main.c
+++ src/main.c
@@ -13,12 +13,40 @@
 #include "minishell.h"
 
 /*
+** Prints a welcome message
+*/
+
+static void		print_welcome(char *path, char *name)
+{
+	printf("%s\n", path);
+	printf("%s\n", name);
+	return ;
+}
+
+/*
+** Return the number of entries in a table
+*/
+
+static size_t	tablen(char **tab)
+{
+	int			len;
+
+	len  = 0;
+	while(tab[len] != NULL)
+		len += 1;
+	return (len);
+}
+
+/*
 ** Main function.
 ** Use third argument instead of -> extern char **environ;
 */
 
-int			main(int argc, char *argv[], char *envp[])
+int			main(int argc, char *argv[], char *envp[], char *apple[])
 {
+	char	**env;
+
+	print_welcome(apple[0], argv[0]);
+	return (minishell(env, argv[0]));
 	(void)argc;
-	return (minishell(envp, argv[0]));
 }
