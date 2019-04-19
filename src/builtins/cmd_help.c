811c8518d63bb36c4df5a0798ae06fc233446ffc
jraleman <jraleman@bendiburg.com>
2019-04-19 10:13:54 -0700

    Builtin help command implementation

--- src/builtins/cmd_help.c
+++ src/builtins/cmd_help.c
@@ -13,12 +13,34 @@
 #include "minishell.h"
 
 /*
+** Print Minishell ascii art.
+*/
+
+static void	draw_ascii_art(void)
+{
+	ft_putchar('\n');
+	ft_putendl(",-.-.o     o     |         |    |    ");
+	ft_putendl("| | |.,---..,---.|---.,---.|    |    ");
+	ft_putendl("| | |||   ||`---.|   ||---'|    |    ");
+	ft_putendl("` ' '``   '``---'`   '`---'`---'`---'");
+	return ;
+}
+
+/*
 ** Builtin help message command implementation.
 */
 
 char		**cmd_help(char **args, char **env, char *name)
 {
-	ft_putendl("Help");
+	int		i;
+
+	i = -1;
+	draw_ascii_art();
+	ft_putendl("By: Jose Ramon Aleman\n\n");
+	ft_putendl("Type program names and arguments, and hit enter.");
+	ft_putendl("The following are built in:");
+	while (++i < BLT_NUM)
+		ft_putendl(blt_str(i));
 	return (env);
 	(void)args;
 	(void)name;
