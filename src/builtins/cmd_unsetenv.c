a6541101eae273089d6f32127f2463a83be7bb9b
jraleman <jraleman@bendiburg.com>
2019-04-19 09:41:51 -0700

    Fix unsetenv pwd mem alloc error

--- src/builtins/cmd_unsetenv.c
+++ src/builtins/cmd_unsetenv.c
@@ -24,7 +24,9 @@ char	**cmd_unsetenv(char **args, char **env, char *name)
 	i = -1;
 	len = ft_strlen(args[1]);
 	(args[1]) ? (args[1] += (args[1][0] == '$')) : 0;
-	if (args[1] && ft_find_env(args[1], env)[0] != 0)
+	if (!ft_strcmp(args[1], "PWD"))
+		ft_putendl("I can't let you do that...");
+	else if (args[1] && ft_find_env(args[1], env)[0] != 0)
 	{
 		while (env[++i])
 			if (!ft_strncmp(env[i], args[1], len) && env[i][len] == '=')
